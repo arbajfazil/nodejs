1f8763528da13436073ae81806cff7ffd0e04fdf
arbajfazil <4arbajalam@gmail.com>
2026-08-02 16:57:23 +0530

    project 1 index.js

---
+++ PROJECT-01/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+
+const app = express();
+const port = 8000;
+
+app.get('/users',(req,res)=>{
+    const html = `
+    <ul>
+        ${users.map((user)=> `<li> ${user.first_name}</li>`).join("")}
+    </ul>
+  `
+  res.send(html);
+})
+
+//REST API
+app.get('/api/users',(req,res)=>{
+    return res.json(users)
+})
+app.route('/api/users/:id')
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id)
+    return res.json(user)
+})
+.put((req,res)=>{
+    return res.json({status:"pending"})
+})
+.delete((req,res)=>{
+    return res.json({status:"pending"})
+})
+
+app.listen(port,()=>console.log("server started"));
