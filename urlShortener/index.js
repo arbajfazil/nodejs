6297167280f8d19c7701be224354d8d8aa37e003
arbajfazil <4arbajalam@gmail.com>
2026-08-11 22:16:58 +0530

    index.js

--- urlShortener/index.js
+++ urlShortener/index.js
@@ -1,12 +1,42 @@
 const express = require("express");
+const path = require("path")
 const urlRoutes = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
 const {connectToMongoDb} = require("./connect")
+const URL = require("./models/url");
 const app = express();
 
 const port = 8001;
 connectToMongoDb('mongodb://localhost:27017/short-url')
 .then(()=>console.log("mongodb connected"))
 
+app.set("view engine", "ejs")
+app.set("views", path.resolve('./views'))
 app.use(express.json())
+
+app.get('/test',async(req,res)=>{
+    const allUrls = await URL.find({})
+    return res.render('home',{
+        urls:allUrls
+    })
+})
+app.use(express.urlencoded({extended:false}))
 app.use('/url',urlRoutes)
+app.use('/',staticRoute)
+app.get('/:shortid',async(req,res)=>{
+    const shortId = req.params.shortid;
+    const entry= await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+        visitHistory:{
+            timestamp:Date.now()
+        }
+    }})
+    if (!entry) {
+        return res.status(404).json({
+            message: "Short URL not found"
+        });
+    }
+    res.redirect(entry.redirectURL)
+})
 app.listen(port,()=>console.log(`server started at PORT ${port}`))
